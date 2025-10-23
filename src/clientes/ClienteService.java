bc30ee83299f68afbd6bc75bc7637481f78745af
IARA DE MARCO MAIA <iara_maia@estudante.sesisenai.org.br>
2025-10-23 19:03:17 -0300

    3

--- src/clientes/ClienteService.java
+++ src/clientes/ClienteService.java
@@ -1,4 +1,48 @@
 package clientes;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ClienteService {
+    private List<Cliente> clientes = new ArrayList<>();
+
+
+    public void cadastrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+
+    public Cliente consultarCliente(int id) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getId() == id) {
+                return cliente;
+            }
+        }
+        System.out.println("Cliente não encontrado.");
+        return null;
+    }
+
+
+    public void excluirCliente(int id) {
+        Cliente cliente = consultarCliente(id);
+        if (cliente != null) {
+            clientes.remove(cliente);
+        } else {
+            System.out.println("Cliente não encontrado.");
+        }
+    }
+
+
+    public void listarClientes() {
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente);
+        }
+    }
 }
+
+
+
+
+
